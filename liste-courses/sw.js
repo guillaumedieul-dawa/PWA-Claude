b1585c7cd87558280451293b538b3fc7ca9659a6
guillaumedieul-dawa <guillaume.dieul@gmail.com>
2026-02-27 23:39:34 +0100

    Add files via upload

---
+++ liste-courses/sw.js
@@ -0,0 +1,28 @@
+const CACHE = 'listecourses-v2';
+const ASSETS = ['./', './index.html', './manifest.json',
+  '../icons/courses-192.svg', '../icons/courses-512.svg'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== CACHE).map(k => caches.delete(k))
+    )).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      const network = fetch(e.request).then(res => {
+        if (res.ok) caches.open(CACHE).then(c => c.put(e.request, res.clone()));
+        return res;
+      }).catch(() => cached || caches.match('./index.html'));
+      return cached || network;
+    })
+  );
+});

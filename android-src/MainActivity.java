e8d2c44646bc6f9818c5b57601793eef9f7bbaf5
guillaumedieul-dawa <guillaume.dieul@gmail.com>
2026-06-06 13:03:55 +0200

    Update MainActivity.java

--- android-src/MainActivity.java
+++ android-src/MainActivity.java
@@ -1,12 +1,8 @@
 package com.famille.dieulgandet;
 
 import com.getcapacitor.BridgeActivity;
-import android.os.Bundle;
 
 public class MainActivity extends BridgeActivity {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        registerPlugin(SmsPlugin.class);
-        super.onCreate(savedInstanceState);
-    }
+    // Les plugins avec l'annotation @CapacitorPlugin sont automatiquement enregistrés.
+    // registerPlugin n'est plus requis.
 }
